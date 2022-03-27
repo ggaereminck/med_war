dbc0fd88b6a2ad60ea3af8090252ee656b9afc33
Charlie Gaereminck <ggaereminck@yahoo.com>
2022-03-27 12:58:26 -0400

    Added quarry, forge, stable to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,14 @@
 
 store1 = Store.create()
 
-lumberMill = Store_Building.create(cost: 50, name: 'Lumber Mill', description: 'Produces Wood, collection time is 20 minutes.', unlock_level: 1, store_id: store1.id)
+lumberMill = Store_Building.create(cost: 50, name: 'Lumber Mill', description: 'Produces Wood, collection time is 10 minutes.', unlock_level: 1, store_id: store1.id)
 
-farm = Store_Building.create(cost: 100, name: 'Farm', description: 'Produces Food, collection time is 20 minutes.', unlock_level: 1, store_id: store1.id)
+farm = Store_Building.create(cost: 100, name: 'Farm', description: 'Produces Food, collection time is 10 minutes.', unlock_level: 1, store_id: store1.id)
 
-barracks = Store_Building.create(cost: 150, name: 'Barracks', description: 'Produces Swordsmen, collection time is 20 minutes.', unlock_level: 2, store_id: store1.id)
+barracks = Store_Building.create(cost: 150, name: 'Barracks', description: 'Produces Swordsmen or Spearmen, collection time is 15 minutes.', unlock_level: 2, store_id: store1.id)
 
+quarry = Store_Building.create(cost: 300, name: 'Quarry', description: 'Produces Stone, collection time is 30 minutes.', unlock_level: 2, store_id: store1.id)
+
+forge = Store_Building.create(cost: 300, name: 'Forge', description: 'Produces Iron, collection time is 25 minutes.', unlock_level: 3, store_id: store1.id)
+
+stable = Store_Building.create(cost: 500, name: 'Stable', description: 'Produces Lancers, collection time is 20 minutes')
